dedc78ffbe8abc0f0c02f3a69bc57dea827b0a4c
aayushi221 <aayushipandey221@gmail.com>
2021-08-12 12:33:37 +0530

    shapes animation

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function slideIt() {
+
+    $("#shapes > div").hide();
+
+
+
+    var sizeLoop = $("#shapes > div").length;
+    var startLoop = 0;
+
+    $("#shapes > div").first().eq(startLoop).fadeIn(500);
+
+
+    setInterval(function () {
+
+        $("#shapes > div").eq(startLoop).fadeOut(1000);
+        if (startLoop == sizeLoop - 1) {
+            startLoop = 0
+        }
+        else {
+            startLoop++;
+        }
+
+        $("#shapes > div").eq(startLoop).slideDown(700);
+
+    }, 2000);
+
+
+});
